31a7802e15e69f1012ad17e2be0c8299074b0a8b
Dhruv Singh <dhruvsingh7077@gmail.com>
2026-01-13 22:45:11 +0530

    booking repository adviced

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const { Booking } = require("../models");
-
+// completed the booking repository status 
 class BookingRepository extends CrudRepository {
   constructor() {
     super(Booking);
