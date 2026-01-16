a104a5ed3dd6d8782ba207c0d6b396943efd566f
Dhruv Singh <dhruvsingh7077@gmail.com>
2026-01-16 20:18:25 +0530

    added transactions

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,1 +1,24 @@
-module.exports = {};
+const {StatusCodes} = require('http-status-codes')
+const {BookingService} = require('../services');
+const {SuccessResponse,ErrorResponse} = require('../utils/common')
+async function createBooking(req,res) {
+  try{
+    const response = await BookingService.createBooking({
+      flightId: req.body.flightId,
+     userId: req.body.userId,
+     noofSeats: req.body.noofSeats
+    });
+    return res
+      .status(StatusCodes.OK)
+      .json(SuccessResponse(response));
+  } catch(error) {
+    console.error("UPDATE SEATS ERROR:", error);
+
+    return res
+      .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponse(error));
+  }
+}
+module.exports = {
+    createBooking
+};
